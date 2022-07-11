ec5cc93fde79bca24f5cba0442e8947e102d9b4c
Weiqi Mei <weiqimei@ucsb.edu>
2022-07-11 21:16:38 +0000

    need to fix sign up form

--- app/forms/signup_form.py
+++ app/forms/signup_form.py
@@ -21,6 +21,8 @@ def username_exists(form, field):
 
 
 class SignUpForm(FlaskForm):
+    first_name = StringField('first_name')
+    last_name = StringField('last_name')
     username = StringField(
         'username', validators=[DataRequired(), username_exists])
     email = StringField('email', validators=[DataRequired(), user_exists])
