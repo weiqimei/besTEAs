e8d65f8033db7e4ca0110b058df769609b3358d0
Weiqi Mei <weiqimei@ucsb.edu>
2022-07-14 07:27:30 +0000

    added about me

--- react-app/src/components/NavBar.js
+++ react-app/src/components/NavBar.js
@@ -37,13 +37,18 @@ const NavBar = () => {
         <nav>
           <ul className='navbar'>
             <div className='each-nav-element'>
+              <NavLink to='/bobaShops' exact={true} activeClassName='active'>
+                Browse Boba Shops
+              </NavLink>
+            </div>
+            <div className='each-nav-element'>
               <NavLink to='/bobaShops/new'>
                 Add Boba Shop
               </NavLink>
             </div>
             <div className='each-nav-element'>
-              <NavLink to='/bobaShops' exact={true} activeClassName='active'>
-                Boba Shops
+              <NavLink to='/about'>
+                About
               </NavLink>
             </div>
             <div className='each-nav-element'>
